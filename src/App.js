ce4eb48e1ae2a8ebaf4af092babe95e5a29783e7
Etienne Dayre <etiennedayre10@gmail.com>
2021-10-18 21:25:04 +0200

    feat: adding jsx and hooks part of the course

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "my-brand" }, "Adopt Me!"),
-    ...[1, 2, 3, 4].map((i) => React.createElement("h2", {}, i)),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      bread: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      bread: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Sudo",
-      animal: "Dog",
-      bread: "Wheaten Terrier",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
